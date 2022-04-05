1dfbd0f9602493ab3e479a2573aa25276c6a7ca7
Ryan Jones <96364933+Jonesry99@users.noreply.github.com>
2022-04-05 20:37:50 +0100

    Added Live Data feature
    Added a menu option where the user is able to choose between visualising either cached flight data (for offline use) or live data that is updated as frequently as the user chooses to run the program.

--- ryanVis.py
+++ ryanVis.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import flightTourismDataLoader as ftd
 
 
-def ryanNumOfFlightsVis():
-    df = load_cached_data()
+def ryanNumOfFlightsVis(df):
     aircraftCountry = ["US", "CN", "IE", "UK", "IN"]
     values = [0, 0, 0, 0, 0]
     for x in df["flag"]:
@@ -31,11 +31,11 @@ def ryanNumOfFlightsVis():
     plt.show()
 
 
-def ryanScatterMapEU():
-    df = load_cached_data()
+def ryanScatterMapEU(df):
     figure, mapCreate = plt.subplots(figsize=(10, 10))
     boundingBox = (-25.12, 48.20, 35.99, 67.39)
     mapEU = plt.imread('mapEurope.png')
+    mapCreate.set_suptitle = ("European Flights Heatmap")
     mapCreate.scatter(df.lng, df.lat, alpha=0.2, c='#ff03cd')
     mapCreate.set_xlim(boundingBox[0], boundingBox[1])
     mapCreate.set_ylim(boundingBox[2], boundingBox[3])
@@ -49,7 +49,31 @@ def load_cached_data():
     return pd.read_csv('cachedFlightData.csv')
 
 
+def load_live_data():
+    return ftd.save_flight_data(flight_api_key)
+
+def dataChoice():
+    choice = int(input("Please choose 1 of the following:\n"
+                       "1. Cached Flight Data\n"
+                       "2. Live Flight Data\n"
+                       "?: "))
+    try:
+        if choice == 1:
+            ryanNumOfFlightsVis(load_cached_data())
+            ryanScatterMapEU(load_cached_data())
+        elif choice ==2:
+            ryanNumOfFlightsVis(load_live_data())
+            ryanScatterMapEU(load_live_data())
+        else:
+            print("Invalid choice, please try again\n")
+            dataChoice()
+    except ValueError:
+        print("Please enter a numerical input as your choice\n")
+        dataChoice()
+
+
+
 if __name__ == "__main__":
     flight_api_key = "018ec34c-8a03-4cd6-aa66-026d1a0385cf"
-    ryanNumOfFlightsVis()
-    ryanScatterMapEU()
+    dataChoice()
+
